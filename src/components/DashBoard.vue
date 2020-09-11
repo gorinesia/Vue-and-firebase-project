322f71697144705f89feb66da537ab5c05f63bb0
Akinori Sakakibara <gorion.1213@gmail.com>
2020-09-11 16:20:20 +0900

    modified the source cord with vuex

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -11,10 +11,6 @@
       <button @click="signOut">サインアウト</button>
     </div>
     <div>
-      <button @click="updateButton">更新</button>
-      <button @click="confirmButton">確認</button>
-    </div>
-    <div>
       <hr>
       <ul>ユーザー名
         <li v-for="(user, index) in loginUsers" :key="index">
@@ -26,72 +22,43 @@
 </template>
 
 <script>
-import firebase from 'firebase'
+// import firebase from 'firebase'
 
 export default {
   data() {
     return {
-      name: '',
-      barance: '',
-      currentUser: {},
-      loginUsers: [],
+      name: this.$store.state.name,
+      barance: this.$store.state.barance,
+      // currentUser: {},
+      currentUser: this.$store.state.currentUser,
+      loginUsers: this.$store.state.loginUsers,
     };
   },
-  created() {
-    this.currentUser = firebase.auth().currentUser;
-    this.name = this.currentUser.displayName;
-    const db = firebase.firestore();
-    db.collection('users')
-      .where('displayName', '!=', this.currentUser.displayName)
-      .get()
-      .then((snapshot) => {
-        const otherUsers = [];
-        snapshot.forEach(doc => {
-          // otherUsers.push(doc.data().displayName)
-          otherUsers.push(doc.data())
-          // console.log(doc.id, '=>', doc.data());
-          this.loginUsers = otherUsers
-          this.barance = doc.data().wallet;
-      })
+  async mounted() {
+    await this.$store.dispatch('createdUser', {
+      displayName: this.$store.state.currentUser.displayName,
+      // barance: this.barance
     })
+    // this.currentUser = firebase.auth().currentUser;
+    // this.name = this.currentUser.displayName;
+    // const db = firebase.firestore();
+    // db.collection('users')
+    //   .where('displayName', '!=', this.currentUser.displayName)
+    //   .get()
+    //   .then((snapshot) => {
+    //     const otherUsers = [];
+    //     snapshot.forEach(doc => {
+    //       otherUsers.push(doc.data())
+    //       this.loginUsers = otherUsers
+    //       this.barance = doc.data().wallet;
+    //   })
+    // })
   },
   methods: {
     signOut() {
       this.$store.dispatch("signOutAction");
-    },
-    async updateButton() {
-      const db = firebase.firestore();
-      // await db.collection('users').add({
-      //   wallet: null
-      // })
-      // .then(doc => {
-      //   console.log(doc)
-      //   db.collection('users').doc(doc.id).update({
-      //     wallet: doc.id
-      //   })
-      // })
-
-      await db.collection('users').doc('vs2GqF2uG9CkAKZq6ECq').update({
-        wallet: 7770
-      })
-      // .then
-      // await db.collection('users').doc('vs2GqF2uG9CkAKZq6ECq')
-      // .onSnapshot((doc) => {
-      //   console.log('Current data: ', doc.data());
-      // })
-    },
-    async confirmButton() {
-      const db = firebase.firestore();
-      await db.collection('users').onSnapshot((querySnapshot) => {
-        console.log('追加！！！！');
-        console.log(querySnapshot)
-        querySnapshot.forEach(doc => {
-          console.log(doc.id, doc.data());
-          console.log('--------------');
-        })
-      })
     }
-  },
+  }
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -8,19 +8,41 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     user: null,
+    // displayName: '',
+    name: '',
+    barance: '',
+    currentUser: {
+      displayName: '',
+      // wallet: '',
+    },
+    loginUsers: [],
   },
   mutations: {
     setUser(state, user) {
+      console.log(state,user)
       state.user = user
-    }
+    },
+    setCurrentUser(state) {
+      console.log(state)
+      state.name = firebase.auth().currentUser.displayName;
+      // state.barance = firebase.auth().currentUser.wallet
+      console.log(state.name)
+    },
+    // setName(state, displayName) {
+    //   console.log(state, displayName)
+    //   state.name = displayName;
+    //   console.log(state.name)
+    // }
   },
   actions: {
     signUpAction({commit}, payload) {
       firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
         .then((result) => {
+          console.log(result)
           result.user.updateProfile({
             displayName: payload.displayName
           })
+          // commit('setName', payload.displayName)
           commit('setUser', result.user.uid)
           router.push('/signin')
         })
@@ -32,12 +54,33 @@ export default new Vuex.Store({
       firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
         .then((result) => {
           commit('setUser', result.user.uid)
-          console.log(result)
           router.push('/dashBoard')
         })
         .catch((error) => {
           console.log(error.message)
         })
+    },
+    createdUser(context, currentUser) {
+      console.log(context, currentUser)
+      // state.currentUser = firebase.auth().currentUser;
+      // state.name = state.currentUser.displayName;
+      context.commit('setCurrentUser', currentUser)
+      console.log(currentUser)
+      // commit('setName', payload.displayName)
+      const db = firebase.firestore();
+      db.collection('users')
+        .where('displayName', '!=', currentUser.displayName)
+        .get()
+        .then((snapshot) => {
+          console.log(snapshot)
+          const otherUsers = [];
+          snapshot.forEach(doc => {
+            otherUsers.push(doc.data())
+            console.log(doc.data())
+            context.state.loginUsers = otherUsers
+            context.state.barance = doc.data().wallet;
+        })
+      })
     },
     signOutAction({commit}) {
       firebase.auth().signOut()
